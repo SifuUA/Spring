61df1b5aa8fb1debd985bd55c8b6e751523ac0ad
SifuUA <alekseykres@gmail.com>
2017-06-24 19:37:08 +0300

    creat many xml files

--- SpringProject_version_2/src/main/java/start/Start.java
+++ SpringProject_version_2/src/main/java/start/Start.java
@@ -9,7 +9,7 @@ import robot.ModelT1000;
  */
 public class Start {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
         ModelT1000 t1000 = (ModelT1000)context.getBean("t1000");
             //t1000.action();
            System.out.println(t1000.getHead());
