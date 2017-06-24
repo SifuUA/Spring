60ea9237f90c3edea6a6e48ded1b64fe3121b80c
SifuUA <alekseykres@gmail.com>
2017-06-24 11:09:02 +0300

    some changes

--- SpringProject/src/main/java/robot/ModelT1000.java
+++ SpringProject/src/main/java/robot/ModelT1000.java
@@ -14,11 +14,30 @@ public class ModelT1000 implements Robot{
     private Leg leg;
     private Head head;
 
+    private String color;
+    private int year;
+    private boolean soundEnabled;
+
+    public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnabled) {
+        this.hand = hand;
+        this.leg = leg;
+        this.head = head;
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
     public ModelT1000(Hand hand, Leg leg, Head head) {
         super();
         this.hand = hand;
         this.leg = leg;
         this.head = head;
+    }
+
+    public ModelT1000(String color, int year, boolean soundEnabled) {
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
     }
 
     public ModelT1000() {
@@ -29,6 +48,9 @@ public class ModelT1000 implements Robot{
         head.calc();
         hand.take();
         leg.go();
+        System.out.println("color:" + color);
+        System.out.println("year:" + year);
+        System.out.println("can play sound:" + soundEnabled);
     }
 
     @Override
@@ -58,5 +80,29 @@ public class ModelT1000 implements Robot{
 
     public void setHead(Head head) {
         this.head = head;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isSoundEnabled() {
+        return soundEnabled;
+    }
+
+    public void setSoundEnabled(boolean soundEnabled) {
+        this.soundEnabled = soundEnabled;
     }
 }
--- SpringProject/src/main/java/start/Start.java
+++ SpringProject/src/main/java/start/Start.java
@@ -11,12 +11,11 @@ public class Start {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
-        Object object = context.getBean("t1000");
+        Object object = context.getBean("t1000Empty");
 
         if (object instanceof ModelT1000) {
             ModelT1000 t1000 = (ModelT1000) object;
             t1000.action();
-            t1000.dance();
         }
     }
 }
