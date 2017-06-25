7d2b2e080042860d3aec23a000584faefa8542a6
SifuUA <alekseykres@gmail.com>
2017-06-25 10:04:17 +0300

    method injection

--- SpringProject_version_3/src/main/java/robot/ModelT1000.java
+++ SpringProject_version_3/src/main/java/robot/ModelT1000.java
@@ -3,85 +3,59 @@ package robot;
 import interfaces.Hand;
 import interfaces.Head;
 import interfaces.Leg;
-import interfaces.Robot;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
  * Created by Alex on 23.06.2017.
  */
-public class ModelT1000 implements Robot, InitializingBean, DisposableBean{
-
-    private Hand hand;
-    private Leg leg;
-    private Head head;
+public class ModelT1000 extends BaseModel implements InitializingBean, DisposableBean{
 
     private String color;
     private int year;
     private boolean soundEnabled;
 
-    public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnabled) {
-        this.hand = hand;
-        this.leg = leg;
-        this.head = head;
-        this.color = color;
-        this.year = year;
-        this.soundEnabled = soundEnabled;
+    public ModelT1000(){
     }
 
     public ModelT1000(Hand hand, Leg leg, Head head) {
-        super();
-        this.hand = hand;
-        this.leg = leg;
-        this.head = head;
+        super(hand, leg, head);
     }
 
-    public ModelT1000(String color, int year, boolean soundEnabled) {
+    public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnabled) {
+        super(hand, leg, head);
         this.color = color;
         this.year = year;
         this.soundEnabled = soundEnabled;
     }
 
-    public ModelT1000() {
+    public ModelT1000(String s, int i, boolean b) {
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println(this + " - method destroy()");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(this + " - method int()");
     }
 
     @Override
     public void action() {
-        head.calc();
-        hand.take();
-        leg.go();
-        System.out.println("color:" + color);
-        System.out.println("year:" + year);
-        System.out.println("can play sound:" + soundEnabled);
+        getHead().calc();
+        getHand().take();
+        getLeg().go();
+        System.out.println("color " + color );
+        System.out.println("year " + year );
+        System.out.println("can play sound " + soundEnabled );
+
     }
 
     @Override
     public void dance() {
-        System.out.println("T1000 is dancing");
-    }
-
-    public Hand getHand() {
-        return hand;
-    }
-
-    public void setHand(Hand hand) {
-        this.hand = hand;
-    }
-
-    public Leg getLeg() {
-        return leg;
-    }
-
-    public void setLeg(Leg leg) {
-        this.leg = leg;
-    }
-
-    public Head getHead() {
-        return head;
-    }
-
-    public void setHead(Head head) {
-        this.head = head;
+        System.out.println("T1000 is dancing!");
     }
 
     public String getColor() {
@@ -106,23 +80,5 @@ public class ModelT1000 implements Robot, InitializingBean, DisposableBean{
 
     public void setSoundEnabled(boolean soundEnabled) {
         this.soundEnabled = soundEnabled;
-    }
-
-    public void initObject(){
-        System.out.println("InitObj");
-    }
-
-    public void destroyObj(){
-        System.out.println("DestroyObj");
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        System.out.println(this + " - method destroy()");
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println(this + " - method int()");
     }
 }
--- SpringProject_version_3/src/main/java/start/Start.java
+++ SpringProject_version_3/src/main/java/start/Start.java
@@ -1,5 +1,7 @@
 package start;
 
+import interfaces.Robot;
+import interfaces.RobotConveer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import robot.ModelT1000;
@@ -10,7 +12,21 @@ import robot.ModelT1000;
 public class Start {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
-        ModelT1000 t1000 = (ModelT1000)context.getBean("t1000");
+        RobotConveer t1000Conveer = (RobotConveer) context.getBean("t1000Conveer");
+
+        Robot terminator1 = t1000Conveer.createRobot();
+        Robot terminator2 = t1000Conveer.createRobot();
+        Robot terminator3 = t1000Conveer.createRobot();
+
+        System.out.println("Terminator1 " + terminator1);
+        System.out.println("Terminator2 " + terminator2);
+        System.out.println("Terminator3 " + terminator3);
+
+
+
+
+
+        // ModelT1000 t1000 = (ModelT1000)context.getBean("t1000");
             //t1000.action();
         //   System.out.println(t1000.getHead());
 //            t1000 = (ModelT1000)context.getBean("t1000");
